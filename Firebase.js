d6fdbcac8e4e68d015d2413e30b56c49bce0002c
Vivaan001-creator <vivaan51009@gmail.com>
2026-07-17 21:46:25 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-// Your web app's Firebase configuration
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA83oNjXsBaXPl5jVZgdLm2E1f4MMoFfEM",
   authDomain: "school-management-portal-fb8da.firebaseapp.com",
@@ -13,8 +11,8 @@ const firebaseConfig = {
   appId: "1:271719672244:web:7396de0b78e997dd672ae1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const app = firebase.initializeApp(firebaseConfig);
 
-const db = firebase.firestore();
+const db = getFirestore(app);
+
+export { db };
