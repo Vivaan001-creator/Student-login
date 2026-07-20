940f479d55d1a94bfac8076cec17ee1dbd57843b
Vivaan001-creator <vivaan51009@gmail.com>
2026-07-20 13:35:08 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -822,39 +822,6 @@ document.body.classList.add("loaded");
 
 });
 
-
-// ==========================
-// Dashboard Mouse Effect
-// ==========================
-
-const dashboard = document.querySelector(".dashboard");
-
-if(dashboard){
-
-document.addEventListener("mousemove",(e)=>{
-
-const x =
-(window.innerWidth/2-e.clientX)/45;
-
-const y =
-(window.innerHeight/2-e.clientY)/45;
-
-dashboard.style.transform=
-
-`rotateY(${x}deg) rotateX(${-y}deg)`;
-
-});
-
-document.addEventListener("mouseleave",()=>{
-
-dashboard.style.transform=
-
-"rotateX(0deg) rotateY(0deg)";
-
-});
-
-}
-
 // ==========================
 // Dashboard Date Time
 // ==========================
